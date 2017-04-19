830e15077f3b344718ad7567b55c08d10b080544
Jonathon Gore <jackgore@hotmail.ca>
2017-04-19 19:19:09 -0400

    Updated endpoints to Insert and Updated data.. kind of

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,4 +1,5 @@
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -6,13 +7,18 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.net.InetSocketAddress;
+import java.util.List;
 
 /**
  * Created by jack on 2017-03-18.
  * Singleton server class
  */
 public class Server {
+
+    private static final String HTTP_POST = "POST";
+    private static final String HTTP_GET = "GET";
 
     private static Logger logger = LogManager.getLogger(Server.class);
     private static Server server = null;
@@ -46,17 +52,17 @@ public class Server {
 
     private void createEndpoints() {
         logger.info("Registering endpoints..");
-        httpServer.createContext("/events", new EventCreationHandler());
+        httpServer.createContext("/events", new EventHandler());
         httpServer.createContext("/test", new MyHandler());
     }
 
     // Constructor
     private Server() {
         /*TODO: Instead of passing in null we must pass in the appropriate client
-     *          for retrieving data. This will be elastic search client
+         *      for retrieving data. This will be elastic search client
          */
 
-        eventManager = new EventManager(null);
+        eventManager = new EventManager(new ElasticDataClient());
 
         gson = new Gson();
         // Try to create the server otherwise throw
@@ -87,21 +93,44 @@ public class Server {
     /**
      * Handler for the /events endpoint
      */
-    private class EventCreationHandler implements HttpHandler {
+    private class EventHandler implements HttpHandler {
+
+        /**
+         * Handles a GET request for the events endpoint
+         * @return the String that is to be used as the response
+         */
+        private String handleGET() {
+            logger.info("Received get request at /events");
+            Type listType = new TypeToken<List<Event>>() {}.getType();
+            String response = gson.toJson(eventManager.getEvents(), listType);
+            logger.info("Sending response: {}", response);
+            return response;
+        }
+
+        /**
+         * Handles a POST request for the events endpoint
+         * @return the String that is to be used as the response
+         */
+        private String handlePOST(HttpExchange t) {
+            // Convert Body of the Http request to string
+            String body = Utilities.toString(t.getRequestBody());
+            logger.info("Received post request at /events. With body {}", body);
+            eventManager.createEvent(body);
+            return "Created event successfully";
+        }
+
         @Override
         public void handle(HttpExchange t) throws IOException {
+            String response = "";
             // Get body from http request
-            if(getRequestType(t).equalsIgnoreCase("POST")) {
-                // Body of the Http request
-                String body = Utilities.toString(t.getRequestBody());
-                logger.info("Received post request at /events. With body {}", body);
-            } else if(getRequestType(t).equalsIgnoreCase("GET")) {
-                logger.info("Received get request at /events");
+            String requestType = getRequestType(t);
+            if(HTTP_POST.equalsIgnoreCase(requestType)) {
+                response = handlePOST(t);
+
+            } else if(HTTP_GET.equalsIgnoreCase(requestType)) {
+                response = handleGET();
             }
-               // String body = t.getRequestBody().toString();
-               // String e = eventManager.createEvent(body).toString();
-               // System.out.println("Event as string:" + e);
-                ResponseWriter.write(t, new Response(200, "This is the response"));
+            ResponseWriter.write(t, new Response(200, response));
         }
     }
 }
