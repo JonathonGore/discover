34834a78850f7e23daa2399dab6bcd5cba7a6797
Jonathon Gore <jackgore@hotmail.ca>
2017-04-26 21:57:22 -0400

    Added JUnit and set up a first 'dummy' test

---
+++ src/test/java/UtilitiesTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by jack on 2017-04-26.
+ */
+public class UtilitiesTest {
+
+    @Test
+    public void queryToMapTest() {
+        assertEquals(true, true);
+    }
+}
